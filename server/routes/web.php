9253467fad6e2b9e86f10f5da30a00af18b9fc66
armendzz <armendzekjiri@gmail.com>
2020-10-12 00:39:29 +0200

    a

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ListController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::apiResource('/list', ListController::class);
+Route::apiResource('/category', CategoryController::class);
